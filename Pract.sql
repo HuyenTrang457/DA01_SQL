b7c589bf79d8f8e72c9c67bb2ee2b8628d04b63d
HuyenTrang457 <155938605+HuyenTrang457@users.noreply.github.com>
2024-01-09 08:38:55 +0700

    Pract.sql

--- Pract.sql
+++ Pract.sql
@@ -1,4 +1,11 @@
---ex1
-LECT NAME FROM CITY
-WHERE POPULATION >120000
-AND COUNTRYCODE= 'USA'
+--GROUP BY, HAVING
+
+select customer_id, DATE(payment_date),
+round(avg(amount),3) as average_amount,
+count(payment_id) as amount_payment
+from payment
+where (DATE(payment_date) between '2020-04-28' and '2020-05-01')
+
+group by customer_id, DATE(payment_date)
+having count(payment_id)>1
+order by customer_id  desc
