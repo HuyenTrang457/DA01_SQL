8bd951f8b9bc90964177285981b4b73a676ab3f2
HuyenTrang457 <155938605+HuyenTrang457@users.noreply.github.com>
2024-01-22 16:16:50 +0700

    Update Practice5.sql

--- Practice5.sql
+++ Practice5.sql
@@ -45,4 +45,9 @@ JOIN Orders as o ON p.product_id=o.product_id
 WHERE o.order_date BETWEEN '2020-02-01' AND '2020-02-29'
 GROUP BY p.product_name
 HAVING SUM(o.unit)>=100
-
+--EX7
+SELECT a.page_id 
+FROM pages AS a
+LEFT JOIN page_likes AS b ON a.page_id=b.page_id
+WHERE b.liked_date IS NULL
+ORDER BY a.page_id 
