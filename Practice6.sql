ac98b8d5a4120efc35686e411a661225f37200e3
HuyenTrang457 <155938605+HuyenTrang457@users.noreply.github.com>
2024-01-25 09:38:06 +0700

    Update Practice6.sql

--- Practice6.sql
+++ Practice6.sql
@@ -9,5 +9,30 @@ WITH count_dupl AS
   SELECT COUNT(company_count) as duplicate_companies
   FROM count_dupl
   WHERE company_count=2
- 
+
+--EX3
+WITH CTE_amount as
+(SELECT policy_holder_id,
+COUNT(case_id)  AS amount_case
+FROM callers
+GROUP BY policy_holder_id
+)
+
+SELECT COUNT(policy_holder_id)
+FROM CTE_amount
+WHERE amount_case >=3
+
+--EX4
+ SELECT a.page_id
+ FROM pages a
+ LEFT JOIN page_likes b ON a.page_id=b.page_id
+ WHERE user_id is NULL
+ ORDER BY a.page_id 
+--EX4/ cach2
+ SELECT page_id
+ FROM pages 
+ WHERE page_id NOT IN (
+    SELECT page_id FROM page_likes
+ )
+
   
