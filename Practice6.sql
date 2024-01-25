61bee867bfdb334d3de13c610c1384516ba6f130
HuyenTrang457 <155938605+HuyenTrang457@users.noreply.github.com>
2024-01-25 23:04:19 +0700

    Update Practice6.sql

--- Practice6.sql
+++ Practice6.sql
@@ -13,43 +13,55 @@ WITH count_dupl AS
 --EX3
 WITH CTE_amount as
 (SELECT policy_holder_id,
-COUNT(case_id)  AS amount_case
+  COUNT(case_id)  AS amount_case
 FROM callers
 GROUP BY policy_holder_id
 )
 
 SELECT COUNT(policy_holder_id)
 FROM CTE_amount
-WHERE amount_case >=3
+  WHERE amount_case >=3
 
 --EX4
  SELECT a.page_id
  FROM pages a
  LEFT JOIN page_likes b ON a.page_id=b.page_id
- WHERE user_id is NULL
+   WHERE user_id is NULL
  ORDER BY a.page_id 
 --EX4/ cach2
  SELECT page_id
  FROM pages 
- WHERE page_id NOT IN (
+   WHERE page_id NOT IN (
     SELECT page_id FROM page_likes
  )
 --EX5
 WITH curr_d AS
 (SELECT user_id,event_date
 FROM user_actions
-WHERE EXTRACT(MONTH from event_date)='7'),
+  WHERE EXTRACT(MONTH from event_date)='7'),
 
 past_d AS 
 (SELECT user_id,event_date
 FROM user_actions
-WHERE EXTRACT(MONTH from event_date)='6'
+  WHERE EXTRACT(MONTH from event_date)='6'
 )
 
 SELECT EXTRACT(MONTH from a.event_date) AS mth,
-COUNT(DISTINCT a.user_id) monthly_active_users
-
+    COUNT(DISTINCT a.user_id) monthly_active_users
 FROM curr_d a
-JOIN past_d b ON a.user_id=b.user_id
-GROUP BY EXTRACT(MONTH from a.event_date)
-  
+  JOIN past_d b ON a.user_id=b.user_id
+  GROUP BY EXTRACT(MONTH from a.event_date)
+
+--EX6
+SELECT SUBSTRING(trans_date,1,7 ) AS month, country,  
+COUNT(id) trans_count, 
+SUM(CASE
+        WHEN state='approved' THEN 1 ELSE 0
+END) AS approved_count,
+SUM(amount)AS trans_total_amount, 
+SUM(CASE
+        WHEN state='approved' THEN amount ELSE 0
+END) AS approved_total_amount
+FROM Transactions
+GROUP BY country, SUBSTRING(trans_date,1,7 )
+
