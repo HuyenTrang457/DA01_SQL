4a458793f057d30082ee50ac3610432d0365ceb3
HuyenTrang457 <155938605+HuyenTrang457@users.noreply.github.com>
2024-01-26 20:14:22 +0700

    Update Practice6.sql

--- Practice6.sql
+++ Practice6.sql
@@ -114,6 +114,15 @@ UNION ALL
         LIMIT 1
 )
 
+--EX12
+WITH CTE_A AS (SELECT requester_id FROM RequestAccepted
+UNION ALL
+    SELECT accepter_id FROM RequestAccepted)
+SELECT requester_id AS id,COUNT(*) AS num
+FROM CTE_A
+    GROUP BY requester_id
+ORDER BY num DESC
+    LIMIT 1
 
 
 
