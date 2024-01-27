db919d1ce74f4b8de39964060044ee6fc79b390f
HuyenTrang457 <155938605+HuyenTrang457@users.noreply.github.com>
2024-01-27 22:40:11 +0700

    Update PRACTICE_7.sql

--- PRACTICE_7.sql
+++ PRACTICE_7.sql
@@ -24,3 +24,12 @@ FROM (SELECT user_id,spend, transaction_date,
       FROM transactions) AS M
 WHERE M.stt=3
 
+--EX4
+SELECT transaction_date,user_id,purchase_count
+FROM 
+  (SELECT transaction_date,user_id,
+      COUNT(*) OVER(PARTITION BY user_id,DATE(transaction_date)) AS purchase_count, 
+      ROW_NUMBER() OVER(PARTITION BY user_id ORDER BY transaction_date DESC) AS stt
+  FROM user_transactions) AS m
+  WHERE m.stt=1
+  ORDER BY transaction_date
