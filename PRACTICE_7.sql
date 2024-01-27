69a54719817c535f2514da8355fe339d29432d2a
HuyenTrang457 <155938605+HuyenTrang457@users.noreply.github.com>
2024-01-27 21:58:25 +0700

    Create PRACTICE_7.sql

---
+++ PRACTICE_7.sql
@@ -0,0 +1,26 @@
+--EX1
+WITH CTE_A AS 
+  (SELECT EXTRACT(YEAR FROM transaction_date) AS year, product_id, spend AS curr_year_spend,
+      LAG(spend) OVER(PARTITION BY product_id ORDER BY EXTRACT(YEAR FROM transaction_date))
+          AS prev_year_spend
+  FROM user_transactions)
+    
+SELECT year, product_id,curr_year_spend,prev_year_spend,
+    ROUND(100*((curr_year_spend-prev_year_spend)/prev_year_spend),2) AS yoy_rate
+FROM CTE_A
+
+--EX2
+SELECT card_name,issued_amount
+FROM
+  (SELECT card_name,issued_amount,
+      ROW_NUMBER() OVER (PARTITION BY issue_year ORDER BY issue_month) AS stt
+  FROM monthly_cards_issued) AS m
+  WHERE m.stt=1
+
+--EX3
+SELECT user_id,spend,transaction_date
+FROM (SELECT user_id,spend, transaction_date,
+          ROW_NUMBER() OVER(PARTITION BY user_id ORDER BY transaction_date) AS stt
+      FROM transactions) AS M
+WHERE M.stt=3
+
