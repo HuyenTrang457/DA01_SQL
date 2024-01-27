e4852ae3fdc551c33ea173f8d8a719101b6a1857
HuyenTrang457 <155938605+HuyenTrang457@users.noreply.github.com>
2024-01-28 00:08:04 +0700

    Update PRACTICE_7.sql

--- PRACTICE_7.sql
+++ PRACTICE_7.sql
@@ -33,3 +33,17 @@ FROM
   FROM user_transactions) AS m
   WHERE m.stt=1
   ORDER BY transaction_date
+
+
+--EX8
+WITH CTE_A AS (SELECT a.artist_name,
+                    DENSE_RANK() OVER(ORDER BY COUNT(*) DESC ) AS artist_rank
+              FROM artists AS a  
+                JOIN songs AS b ON a.artist_id=b.artist_id
+                JOIN global_song_rank AS c ON c.song_id=b.song_id
+              WHERE c.rank<=10
+              GROUP BY a.artist_name)
+
+SELECT artist_name,artist_rank
+FROM CTE_A
+WHERE artist_rank<=5
