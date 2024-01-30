f8e6072fd20a7ab70d9ad594c60e784ecf29f6bb
HuyenTrang457 <155938605+HuyenTrang457@users.noreply.github.com>
2024-01-30 09:09:20 +0700

    Update PRACTICE_8.sql

--- PRACTICE_8.sql
+++ PRACTICE_8.sql
@@ -11,3 +11,12 @@ FROM Delivery
 GROUP BY customer_id)
 
 --EX2
+--EX3
+SELECT 
+CASE
+        WHEN id=(SELECT MAX(id) FROM Seat) AND id%2=1 THEN  id
+        WHEN id%2=1 THEN id+1
+        ELSE id-1
+END AS  id,student
+FROM Seat
+ORDER BY id
