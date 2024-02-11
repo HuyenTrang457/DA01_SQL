a4bb1fa7ea64a13080a81c2ef3761179ae382898
HuyenTrang457 <155938605+HuyenTrang457@users.noreply.github.com>
2024-02-11 23:20:07 +0700

    Update Project_2.sql

--- Project_2.sql
+++ Project_2.sql
@@ -23,16 +23,37 @@ GROUP BY  1
 ORDER BY  1
 
  
---2
- WITH CTE AS(SELECT *,
- DENSE_RANK() OVER(PARTITION BY gender ORDER BY age) AS stt
- FROM bigquery-public-data.thelook_ecommerce.users),
+
+
+--3
+WITH CTE AS(SELECT *,
+                 DENSE_RANK() OVER(PARTITION BY gender ORDER BY age) AS stt
+             FROM bigquery-public-data.thelook_ecommerce.users),
 
  CTE_2 AS(SELECT *,
- MIN(stt) OVER(PARTITION BY gender ) AS min_stt ,
- MAX(stt) OVER(PARTITION BY gender )  AS max_stt
- FROM CTE)
- SELECT
- first_name, last_name, gender, age
- FROM CTE_2
- WHERE stt=min_stt OR stt=max_stt
+                 MIN(stt) OVER(PARTITION BY gender ) AS min_stt ,
+                 MAX(stt) OVER(PARTITION BY gender )  AS max_stt
+         FROM CTE)
+SELECT first_name, last_name, gender, age,
+       (CASE WHEN stt=min_stt THEN 'youngest' ELSE 'oldest'END) AS c,
+        COUNT(*) OVER(PARTITION BY gender, age)
+FROM CTE_2 AS a
+ JOIN bigquery-public-data.thelook_ecommerce.order_items AS b ON a.id=b.id
+ WHERE stt IN (max_stt,min_stt) AND FORMAT_DATE( '%Y-%m',b.created_at) BETWEEN '2019-01' AND '2022-04'
+ ORDER BY gender , age 
+
+ /* insight: Male: trẻ nhất là 12t, số lượng: 220
+                    lớn nhất 70t, số lượng: 250
+            Female: trẻ nhất là 12t, số lượng: 211
+                    lớn nhất 70t, số lượng: 218 */
+
+
+
+
+
+
+
+
+
+
+
