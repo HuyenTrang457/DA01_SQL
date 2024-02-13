7aab07ac641975ec454f1e1e97a53dea90332495
HuyenTrang457 <155938605+HuyenTrang457@users.noreply.github.com>
2024-02-14 00:13:37 +0700

    Update Project_2.sql

--- Project_2.sql
+++ Project_2.sql
@@ -3,30 +3,18 @@
 --2.Thống kê giá trị đơn hàng trung bình và tổng số người dùng khác nhau mỗi tháng ( Từ 1/2019-4/2022)
     --insight: distinct_user tăng dần theo thời gian, average_order_value lúc tăng lúc giảm
 
-WITH
-  CTE AS ((
-    SELECT *
-    FROM bigquery-public-data.thelook_ecommerce.order_items
-    WHERE
-      delivered_at IS NOT NULL
-      AND returned_at IS NULL ))
+
 SELECT
   FORMAT_DATE( '%Y-%m',b.created_at) AS month_year,
-  COUNT(a.order_id) AS total_order,
-  COUNT(b.user_id) AS total_user,
   ROUND(SUM(b.sale_price)/COUNT(b.sale_price),2) AS average_order_value,
   COUNT(DISTINCT b.user_id) AS distinct_user,
   
-FROM
-  CTE AS a
-RIGHT JOIN
-  bigquery-public-data.thelook_ecommerce.order_items AS b
-ON a.order_id=b.order_id
+FROM bigquery-public-data.thelook_ecommerce.order_items AS b
 WHERE
   FORMAT_DATE( '%Y-%m',b.created_at) BETWEEN '2019-01' AND '2022-04'
 GROUP BY  1
 ORDER BY  1
-
+--------
 
 
 
