1b7b08ad24fa970adcca89b59479ed02b29ff126
HuyenTrang457 <155938605+HuyenTrang457@users.noreply.github.com>
2024-02-25 23:48:03 +0700

    Update PROJECT_3.sql

--- PROJECT_3.sql
+++ PROJECT_3.sql
@@ -12,11 +12,31 @@ Output: MONTH_ID, REVENUE, ORDER_NUMBER
 SELECT month_id, revenue, order_number
 FROM 
 	(select  month_id,year_id, sum(sales) as revenue, count(ordernumber) as order_number,
-	 rank() over(partition by year_id order by sum(sales),count(ordernumber)) AS stt
+	 rank() over(partition by year_id order by sum(sales) desc ,count(ordernumber) desc) AS stt
 	from sales_dataset_rfm_prj_clean
 	 group by month_id, year_id
 	) AS a
 WHERE stt=1
+
+/*3) Product line nào được bán nhiều ở tháng 11?
+Output: MONTH_ID, REVENUE, ORDER_NUMBER
+*/
+SELECT month_id, revenue, order_number
+FROM 
+	(select  productline,month_id,year_id, sum(sales) as revenue, count(ordernumber) as order_number,
+	 rank() over(partition by year_id order by sum(sales) desc ,count(ordernumber) desc) AS stt
+	from sales_dataset_rfm_prj_clean
+	 where month_id=11
+	 group by month_id, year_id,productline
+	 order by year_id, stt
+	) AS a
+WHERE stt=1 
+
+/*4) Đâu là sản phẩm có doanh thu tốt nhất ở UK mỗi năm? 
+Xếp hạng các các doanh thu đó theo từng năm.
+Output: YEAR_ID, PRODUCTLINE,REVENUE, RANK
+*/
+
 
 
 
