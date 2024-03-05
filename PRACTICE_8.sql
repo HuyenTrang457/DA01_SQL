fb7e1be5bd22cb0b285431f967210d0d2cbc12ef
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-03-05 13:04:21 +0700

    Update PRACTICE_8.sql

--- PRACTICE_8.sql
+++ PRACTICE_8.sql
@@ -11,7 +11,7 @@ FROM Delivery
 GROUP BY customer_id)
 
 --EX2
---EX3
+--EX3-----------------------------------------------------------
 SELECT 
 CASE
         WHEN id=(SELECT MAX(id) FROM Seat) AND id%2=1 THEN  id
@@ -20,8 +20,23 @@ CASE
 END AS  id,student
 FROM Seat
 ORDER BY id
+---EX5---------------------------------------------------------
+SELECT round(sum(tiv_2016),2) as tiv_2016
+FROM Insurance
+WHERE  tiv_2015 IN 
+        (select tiv_2015 
+        from insurance 
+        group by tiv_2015
+        having count(*)>1)
 
---EX6
+ AND (lat,lon) IN
+        (select lat,lon
+        from insurance 
+        group by lat,lon
+        having count(*)=1)
+ 
+
+---EX6---------------------------------------------------------
 
 WITH CTE AS (SELECT departmentId,name,salary,
                         DENSE_RANK() OVER(PARTITION BY departmentId ORDER BY salary DESC) AS stt
@@ -30,3 +45,11 @@ SELECT b.name AS Department, a.name AS Employee, a.salary AS Salary
 FROM CTE a
 JOIN Department b ON a.departmentId=b.id
 WHERE a.stt<=3
+---EX7------------------------------------------------------------
+with cte as (select person_name,
+                sum(weight) over(order by turn) as sum 
+        from Queue)
+select person_name from cte
+where sum <=1000
+order by sum desc 
+ limit 1
